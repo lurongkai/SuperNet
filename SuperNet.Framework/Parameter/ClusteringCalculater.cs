bbe1b322a5a359c55f96bd07775029c621f6a086
lurongkai <lengleng3898@gmail.com>
2012-02-17 20:07:49 +0800

    Modify Clustering parameter.

--- SuperNet.Framework/Parameter/ClusteringCalculater.cs
+++ SuperNet.Framework/Parameter/ClusteringCalculater.cs
@@ -10,7 +10,16 @@ namespace SuperNet.Framework.Parameter
     {
         public ClusteringCalculater(Map map) : base(map) { }
 
-        public double CalcClustering(Vertex vertex) {
+        public double CalcClustering() {
+            var sum = 0.0d;
+            foreach (var vertex in _map.AllVertexs) {
+                sum += CalcVertexClustering(vertex);
+            }
+
+            return sum / _map.AllVertexs.Count;
+        }
+
+        private double CalcVertexClustering(Vertex vertex) {
             var vertexDegree = vertex.Edges.Count;
             var vertexCount = _map.AllVertexs.Count;
 
